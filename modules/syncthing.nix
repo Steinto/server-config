2a112464adee08703a58eb5f7d2d4c7573dce3ae
TobySteiner <toby.b.steiner@gmail.com>
2024-09-15 18:12:11 +1200

    fix syntax error

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -10,7 +10,7 @@
       user = "user";
       dataDir = "/home/user/Documents"; # Default folder for new synced folders
       configDir = "/home/user/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
-      guiAddress = "0.0.0.0:8384"
+      guiAddress = "0.0.0.0:8384";
     };
   };
 }
