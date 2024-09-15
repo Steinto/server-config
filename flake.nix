29272f2a504be71d1ad643399fc0826027c1c11b
TobySteiner <toby.b.steiner@gmail.com>
2024-09-15 18:37:37 +1200

    fix syntax error in flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixarr.url = "github:rasmus-kirk"
+    nixarr.url = "github:rasmus-kirk";
     # home-manager = {
     #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
